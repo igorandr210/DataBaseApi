d53e0f5ce7413897fc5ec0403e387a818108aa50
Ihor <i.andrushchenko@intetics.com>
2019-12-05 12:27:23 +0200

    Add DB Handler

--- DataBaseApi/ApplicationContext.cs
+++ DataBaseApi/ApplicationContext.cs
@@ -19,5 +19,33 @@ namespace DataBaseApi
         {
             Database.EnsureCreated();
         }
+
+        public async Task<bool> UpdateItem(ItemModel item)
+        {
+            Items.Update(item);
+            return (await this.SaveChangesAsync()>0);
+        }
+
+        public async Task<bool> AddItem(ItemModel item)
+        {
+            Items.Add(item);
+            return (await this.SaveChangesAsync() > 0);
+        }
+
+        public async Task<bool> DeleteItem(ItemModel item)
+        {
+            Items.Remove(item);
+            return (await this.SaveChangesAsync() > 0);
+        }
+
+        public IEnumerable<ItemModel> GetAllItems()
+        {
+            return Items;
+        }
+
+        public async Task<ItemModel> GetItem(int Id)
+        {
+            return await Items.FirstOrDefaultAsync(x=>x.Id==Id);
+        }
     }
 }
